1f070636901054917fdb46ae3677def45d37d593
willwearing <willwearing@gmail.com>
2021-09-25 13:15:56 -0600

    adding service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,13 @@
 import { Body, Controller, Delete, Get, Param, Post, Req } from '@nestjs/common';
 import { CreateCatDto } from './create-cat-dto';
+import { CatsService } from './cats.service';
+import { Cat } from '../interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {
 
+  }
   /*
   This is how you create a new cat without using a DTO 
   @Post()
@@ -12,14 +16,29 @@ export class CatsController {
   }
   */
 
+  /*   
+  This is how to have a POST request without using a service
   @Post()
   async create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
+  } 
+  */
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
+  /*   
   @Get()
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
+  } 
+  */
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   @Get(':id')
@@ -41,6 +60,8 @@ export class CatsController {
   update(@Param('id') id: string) {
     return `This action updates a #${id} cat`;
   }
+
+
 
   @Delete(':id')
   remove(@Param('id') id: string) {
--- src/cats/create-cat-dto.ts
+++ src/cats/create-cat-dto.ts
@@ -1,5 +1,15 @@
+/* 
+Interfaces and DTO's are roughly the same thing 
+The major difference being: 
+Not only preference, but also use case. Interfaces can not compute properties or use decorators, 
+whereas classes can. NestJS recommends to use classes because you can add decorators from 
+class-validator to properties such as @IsString() or @Max(20). With interfaces such a validation 
+would only be possible outside of the interface context.
+To read more: https://github.com/nestjs/nest/issues/1228
+*/
+
 export class CreateCatDto {
   name: string;
   age: number;
   breed: string;
-}
+} 
